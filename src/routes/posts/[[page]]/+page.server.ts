93f2d9ee1eed6525cd575f5075f905a3b3ae8a10
Mattias Buelens <mattias@buelens.com>
2024-12-29 21:09:42 +0100

    Tweaks

--- src/routes/posts/[[page]]/+page.server.ts
+++ src/routes/posts/[[page]]/+page.server.ts
@@ -4,7 +4,7 @@ import { paginate } from '$lib/util'
 import { error } from '@sveltejs/kit'
 
 export const load: PageServerLoad = async ({ params }) => {
-  const page = params.page ? parseInt(params.page) : 1
+  const page = params.page ? parseInt(params.page, 10) : 1
   const limit = 10
 
   const postsForPage = paginate(posts, { limit, page })
