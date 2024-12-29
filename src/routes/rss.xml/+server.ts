5f779199447157bb1232d3eacbf97bfa3b428785
Mattias Buelens <mattias@buelens.com>
2024-12-29 17:50:13 +0100

    Tweak RSS rendering

--- src/routes/rss.xml/+server.ts
+++ src/routes/rss.xml/+server.ts
@@ -18,24 +18,24 @@ export const GET: RequestHandler = async ({ setHeaders }) => {
     'Content-Type': 'application/xml'
   })
 
-  const indent = ' '.repeat(4)
   const xml = `<rss xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:atom="http://www.w3.org/2005/Atom" version="2.0">
   <channel>
     <title>${name}</title>
     <link>${website}</link>
     <description>${websiteDescription}</description>
     <atom:link href="${website}/rss.xml" rel="self" type="application/rss+xml" />
-    ${posts
-      .map(
-        (post) => `${indent}<item>
+${posts
+  .map(
+    (post) => `\
+    <item>
       <guid>${postsUrl}/${post.slug}</guid>
       <title>${post.title}</title>
       <description>${post.preview.text}</description>
       <link>${postsUrl}/${post.slug}</link>
       <pubDate>${post.date.toUTCString()}</pubDate>
     </item>`
-      )
-      .join('\n')}
+  )
+  .join('\n')}
   </channel>
 </rss>`
 
